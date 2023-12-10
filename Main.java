53b510a043bab85c8ab0339a91a743250fd9cf66
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-12-09 22:30:16 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+class Main{
+  public static void main(String[] args) throws IOException
+  {
+    ToDoList ToDo1 = new ToDoList();
+    ToDo1.setVisible(true);
+  }
+}
